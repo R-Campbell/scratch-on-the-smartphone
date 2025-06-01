87672086b9d53c7f6facf323b8592bfc7a3a1fd4
Rob Campbell <robertcharles.campbell@gmail.com>
2025-06-01 17:43:56 -0400

    move static variables into their own files
    not sure if this'll work with the way it's hosted

---
+++ static/embeddedScripts.js
@@ -0,0 +1,81 @@
+const EMBEDDED_SCRIPTS = {
+    trouble_brewing: {
+        id: 'trouble_brewing',
+        name: 'Trouble Brewing',
+        author: 'Steven Medway & The Pandemonium Institute',
+        description: 'Beginner. Recommended for players and Storytellers new to Blood on the Clocktower or to social deception games.',
+        roles: [
+            'washerwoman', 'librarian', 'investigator', 'chef', 'empath', 'fortune_teller',
+            'undertaker', 'monk', 'ravenkeeper', 'virgin', 'slayer', 'soldier', 'mayor',
+            'butler', 'drunk', 'recluse', 'saint',
+            'poisoner', 'spy', 'scarlet_woman', 'baron',
+            'imp'
+        ],
+        first_night_order: [
+            'poisoner', 'spy', 'washerwoman', 'librarian', 'investigator', 'chef', 'empath', 'fortune_teller', 'butler'
+        ],
+        other_night_order: [
+            'poisoner', 'monk', 'spy', 'scarlet_woman', 'imp', 'ravenkeeper', 'undertaker', 'empath', 'fortune_teller', 'butler'
+        ]
+    },
+    bad_moon_rising: {
+        id: 'bad_moon_rising',
+        name: 'Bad Moon Rising',
+        author: 'Steven Medway & The Pandemonium Institute',
+        description: 'Intermediate. Recommended for players who are proactive, dedicated to working as a team, and don\'t fear dying.',
+        roles: [
+            'grandmother', 'sailor', 'chambermaid', 'exorcist', 'innkeeper', 'gambler',
+            'gossip', 'courtier', 'professor', 'minstrel', 'tea_lady', 'pacifist', 'fool',
+            'goon', 'lunatic', 'tinker', 'moonchild',
+            'godfather', 'devils_advocate', 'assassin', 'mastermind',
+            'zombuul', 'pukka', 'shabaloth', 'po'
+        ],
+        first_night_order: [
+            'lunatic', 'sailor', 'courtier', 'godfather', 'devils_advocate', 'lunatic', 'pukka', 'grandmother', 'chambermaid', 'goon'
+        ],
+        other_night_order: [
+            'minstrel', 'sailor', 'innkeeper', 'courtier', 'gambler', 'devils_advocate', 'lunatic', 'exorcist', 'zombuul', 'pukka', 'shabaloth', 'po', 'assassin', 'godfather', 'professor', 'gossip', 'tinker', 'moonchild', 'grandmother', 'chambermaid', 'goon'
+        ]
+    },
+    sects_and_violets: {
+        id: 'sects_and_violets',
+        name: 'Sects & Violets',
+        author: 'Steven Medway & The Pandemonium Institute',
+        description: 'Intermediate. Recommended for players who want to do wild and unexpected things, pushing the limits of what can be achieved in a bluffing game.',
+        roles: [
+            'clockmaker', 'dreamer', 'snake_charmer', 'mathematician', 'flowergirl', 'town_crier',
+            'oracle', 'savant', 'seamstress', 'philosopher', 'artist', 'juggler', 'sage',
+            'mutant', 'sweetheart', 'barber', 'klutz',
+            'evil_twin', 'witch', 'cerenovus', 'pit_hag',
+            'fang_gu', 'vigormortis', 'no_dashii', 'vortox'
+        ],
+        first_night_order: [
+            'evil_twin', 'philosopher', 'clockmaker', 'dreamer', 'snake_charmer', 'mathematician',
+            'seamstress', 'witch', 'cerenovus'
+        ],
+        other_night_order: [
+            'philosopher', 'dreamer', 'snake_charmer', 'mathematician', 'flowergirl', 'town_crier',
+            'oracle', 'seamstress', 'juggler', 'witch', 'cerenovus', 'pit_hag', 'fang_gu',
+            'vigormortis', 'no_dashii', 'vortox', 'sage', 'sweetheart', 'barber'
+        ]
+    },
+    trouble_babbling: {
+        id: 'trouble_babbling',
+        name: 'Trouble Babbling',
+        author: 'Community Script',
+        description: 'A custom script mixing classic and experimental characters.',
+        roles: [
+            'noble', 'knight', 'investigator', 'chef', 'empath', 'fortune_teller',
+            'soldier', 'monk', 'ravenkeeper', 'philosopher', 'slayer', 'gambler', 'gossip',
+            'butler', 'drunk', 'recluse', 'moonchild',
+            'witch', 'godfather', 'baron', 'scarlet_woman',
+            'imp', 'yaggababble', 'po'
+        ],
+        first_night_order: [
+            'philosopher', 'yaggababble', 'witch', 'godfather', 'imp', 'po', 'knight', 'noble', 'investigator', 'chef', 'empath', 'fortune_teller', 'butler', 'gambler'
+        ],
+        other_night_order: [
+            'philosopher', 'monk', 'witch', 'scarlet_woman', 'imp', 'po', 'yaggababble', 'godfather', 'gossip', 'ravenkeeper', 'empath', 'fortune_teller', 'undertaker', 'butler', 'gambler'
+        ]
+    },
+};
